04b67dc6cc12364112778d0fb71bfcc59a265c82
Heitor França <89424453+Heitorfrc@users.noreply.github.com>
2023-10-30 15:32:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
 print(url)
 
-url_base = url[0:19]
+# Separa base e os parametros
+indice_interrogacao = url.find("?")
+url_base = url[:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[20:35] #len(url) ao inves de 35
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+# Busca o valor de um parametro
+parametro_busca = "moedaOrigem"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find("&", indice_valor)
+if indice_e_comercial == -1 :
+    valor = url_parametros[indice_valor:]
+else :
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
